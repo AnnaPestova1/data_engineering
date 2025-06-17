58e962ce50a0b1d22bfaa4da737575084d48b40f
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2025-06-17 00:04:36 -0400

    streamlit and readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# data_engineering
+**MLB World Series Data Explorer**
+This project is part of the final assignment for the Python course at Code The Dream. It demonstrates end-to-end data handling: from web scraping and cleaning to database management and interactive data visualization.
+
+*Project Components*
+Web Scraping Program
+Scrapes historical World Series data from the Major League Baseball History website. The data is cleaned, assembled into structured pandas DataFrames, and exported to CSV files.
+
+Database Import Program
+Loads the cleaned CSV files into a SQLite database, with each dataset stored in its own table. Foreign key constraints are used to maintain data integrity.
+
+Database Query Program
+A command-line interface that allows users to query the database using JOIN operations to combine and extract insights from different tables.
+
+Dashboard Program
+A Streamlit-based web dashboard that visualizes the World Series results, pitcher biographies, and other data. Users can interactively explore team performances and player stats by year or league.
+
+_Live Demo_
+ Streamlit app: 
+
+_Running Locally_
+To run this project locally:
+
+Clone the repository.
+
+Create and activate a virtual environment.
+
+Install the required libraries:
+
+`pip install -r requirements.txt`
+
+Run the database import and query scripts from the terminal to generate the database.
+
+_Run database query program:_
+
+`python sql_queries.py`
+
+_Launch the Streamlit app:_
+
+`streamlit run streamlit_dashboard.py`
---
+++ dataEngeneering/streamlit_dashboard.py
@@ -0,0 +1,94 @@
+import streamlit as st  
+import pandas as pd   
+import altair as alt
+
+try: 
+    ws_results=pd.read_csv('../cleaned dataframes/world_series_data.csv')
+    nl_results=pd.read_csv('../cleaned dataframes/nl_results.csv')
+    al_results=pd.read_csv('../cleaned dataframes/al_results.csv')
+    ws_pitchers_biography=pd.read_csv('../cleaned dataframes/clean_df_world_series_pitchers_biography.csv')
+    unique_teams_name=pd.read_csv('../cleaned dataframes/unique_teams_name.csv')
+
+
+    # data for search
+    teams = unique_teams_name['team_name'].sort_values()
+    years = ws_results['year']
+    leagues = ['National League', 'American League']
+    colleges = ws_pitchers_biography['college'].unique().tolist()
+    countries = ws_pitchers_biography['birthplace'].unique().tolist()
+    
+    st.sidebar.header('Choose search params')
+    league_index = None
+
+    min_year, max_year = int(years.min()), int(years.max())
+    selected_team = st.sidebar.selectbox('Select Team', teams, placeholder="Select team", index=None) 
+
+    min_year, max_year = st.sidebar.select_slider('Select Year', options=years, value = (min_year, max_year)) 
+    on= st.sidebar.toggle("drop leagues")
+    if on:
+        selected_league = st.sidebar.radio('Select League', leagues, index=league_index)
+    else:
+        selected_league = None
+
+    selected_college = st.sidebar.selectbox('Select College', colleges, placeholder="Select college", index=None) 
+    selected_country = st.sidebar.selectbox('Select Country', countries, placeholder="Select country", index=None) 
+
+    game_table = ws_results[['year', 'nl team', 'nl score', 'al team', 'al score']]
+    game_table= game_table.rename(columns={'nl team': 'NL team', 'nl score': 'NL score', 'al team': 'AL team', 'al score': 'AL score'}, errors='raise')
+    
+    if min_year >1903 & max_year<2024:  
+        game_table = game_table[(game_table['year'] >= min_year) & (game_table['year'] <= max_year)]
+    if selected_league!= None:
+
+        if selected_league == 'National League':
+            game_table = game_table.drop(columns=['AL team', 'AL score'])
+        else:
+            game_table = game_table.drop(columns=['NL team', 'NL score'])
+
+
+    if selected_team!= None:
+        game_table = game_table[(game_table['AL team'] == selected_team) | (game_table['NL team'] == selected_team)]
+       
+    st.title('Some Facts About World Series')
+
+    st.header('World Series games')
+    st.dataframe(game_table, hide_index=True)
+
+    
+    total_pitchers_number = len(ws_pitchers_biography)
+    in_college = ws_pitchers_biography['college'].value_counts().get('None Attended', 0)
+    no_college = total_pitchers_number - in_college
+    college_attendance = pd.DataFrame({'attendance': ['attended', 'non_attended'], 'count':[in_college, no_college]})
+
+    filtered_college_data = ws_pitchers_biography[['name', 'college']]
+
+    filtered_college_data = filtered_college_data[(ws_pitchers_biography['college'] == selected_college)]
+
+    st.header('College attendance')
+
+    chart = alt.Chart(college_attendance).mark_arc().encode(
+    theta=alt.Theta(field="count", type="quantitative"),
+    color=alt.Color(field="attendance", type="nominal"),
+)
+    st.altair_chart(chart)
+
+    st.subheader('Explore college attendance')
+    st.dataframe(filtered_college_data, hide_index=True)
+
+    st.header('Birth Places')
+
+    filtered_birthplace_data = ws_pitchers_biography[['name', 'birthplace']]
+
+    filtered_birthplace_data = filtered_birthplace_data[(ws_pitchers_biography['birthplace'] == selected_country)]
+        
+    birthplace_data = ws_pitchers_biography.groupby('birthplace', as_index=False)['name'].count()
+   
+    birthplace_data= birthplace_data.rename(columns={'name': 'count'}, errors='raise')
+
+    st.line_chart(birthplace_data, x='birthplace', y='count')
+
+    st.subheader('Explore places of birth')
+    st.dataframe(filtered_birthplace_data, hide_index=True)
+except Exception as e:
+        st.toast(f"The error occur: {e}")
+        
